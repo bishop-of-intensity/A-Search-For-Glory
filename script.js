2b5ee9d01b0673690a905c9fc7a1fe4cc8066e16
bishop-of-intensity <79968439+bishop-of-intensity@users.noreply.github.com>
2022-03-31 20:02:17 +0300

    Initial Commit
    :p

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+    $('body').addClass('after-load')
+});
+
+$('.header').click(() => {
+    window.open('https://www.instagram.com/bishop_of_intensity/', '_blank');
+});
+
+$('.aeq_link').click(() => {
+    window.open('https://bishop-of-intensity.github.io/An-Epic-Quest/', '_blank');
+});
+
+$('.bt_link').click(() => {
+    window.open('https://bishop-of-intensity.github.io/Basic-Training/', '_blank');
+});
